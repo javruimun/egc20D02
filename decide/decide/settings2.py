7ba75dfac4d431525eae9a787ccd25daba3b54e3
javruimun <javruimun@alum.us.es>
2020-12-02 18:19:16 +0100

    Añadido cambio para heroku de nuevo

--- decide/decide/settings2.py
+++ decide/decide/settings2.py
@@ -121,14 +121,11 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
